b83f689bf2c52f6c1cd103c4e2074433a67d29ac
Akash Manoj <akashmanoj213@gmail.com>
2024-10-08 12:49:32 +0530

    Updated the request to handle larger files

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { Logger } from 'nestjs-pino';
+import { json, urlencoded } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
@@ -12,6 +13,8 @@ async function bootstrap() {
     }),
   );
 
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ extended: true, limit: '50mb' }));
   app.enableCors();
   await app.listen(parseInt(process.env.PORT) || 8080);
 }
